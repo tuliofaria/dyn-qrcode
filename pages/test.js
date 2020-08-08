3834743ffac8d36f405787ce8382449e1288ba96
Tulio Faria <tuliofaria@gmail.com>
2020-08-08 09:35:49 -0300

    test

--- pages/test.js
+++ pages/test.js
@@ -5,6 +5,10 @@ const Test = () => {
     <div>
       <h1>Test whatsapp:</h1>
       <a href='whatsapp://send?text=Compartilhe isso... url: https://devpleno.com' data-action='share/whatsapp/share'>Compartilhar</a>
+
+      <h2>outro teste:</h2>
+
+      <a href='whatsapp://send?text=Compartilhe isso... url: https://devpleno.com'>Compartilhar</a>
     </div>
   )
 }
