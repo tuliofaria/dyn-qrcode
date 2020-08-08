bc4059131c3160661c9f5655a0a72e694ade8269
Tulio Faria <tuliofaria@gmail.com>
2020-08-08 02:19:34 -0300

    fix

--- pages/test.js
+++ pages/test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Test = () => {
-  (
+  return (
     <div>
       <h1>Test whatsapp:</h1>
       <a href='whatsapp://send?text=Compartilhe isso... url: https://devpleno.com' data-action='share/whatsapp/share'>Compartilhar</a>
